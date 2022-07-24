fb413127e0b1cc9483c4df3b22d4e20e970f4525
Christopher Murphy <murphyc3@oregonstate.edu>
2022-07-24 12:34:00 -0400

    new test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import requests
 
 res = requests.post('http://localhost:5000/playlistgenerator', json={
-    'title': "playlist_title", 
-    'tracks': ["https://open.spotify.com/track/2NtqZmfRIDkXJ2YvY2Kv1F?si=c286783f05554217"]
+    'title': "My playlist", 
+    'tracks': [
+        "https://open.spotify.com/track/2NtqZmfRIDkXJ2YvY2Kv1F?si=c286783f05554217",
+        "https://open.spotify.com/track/4lFO4X6ef61SR6M1KXkSRN?si=55590f077b4a410e",
+        "https://open.spotify.com/track/4pCNJwixy2ImFncaPY2yE2?si=7b57e427de78428c"]
     })
 
 print(res.json())
