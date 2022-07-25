24f09ccb3c2bf02af5c9a5cae5b4063780d703ec
Chris Murphy <murphyc3@oregonstate.edu>
2022-07-25 18:14:21 -0400

    Updating readme

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-res = requests.post('http://localhost:5000/playlistgenerator', json={
+request = requests.post('http://localhost:5000/playlistgenerator', json={
     'title': "My playlist", 
     'tracks': [
         "https://open.spotify.com/track/2NtqZmfRIDkXJ2YvY2Kv1F?si=c286783f05554217",
